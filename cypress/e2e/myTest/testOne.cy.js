442388858690c33a0b98d2c85fbb7f0e0aaa36c0
DarkBen92 <reedan12372@gmail.com>
2023-07-03 18:21:12 +0300

    now-branch

--- cypress/e2e/myTest/testOne.cy.js
+++ cypress/e2e/myTest/testOne.cy.js
@@ -1,4 +1,4 @@
-describe('My First Tests', () => {
+describe('My First Test', () => {
     it('Visiting Google displays the correct title', () => {
       cy.visit('https://google.co.uk')
       cy.title().should('contain', 'Google');
